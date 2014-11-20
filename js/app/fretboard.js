70deead70e83d4b23145379993696d9f367e61e3
jgnatch <nachojgutierrez@gmail.com>
2014-11-20 18:07:31 -0300

    added nuts & frets

--- js/app/fretboard.js
+++ js/app/fretboard.js
@@ -7,6 +7,7 @@
     this.woodLength = 1200;
 
     this.strings = calculateStrings(this.fretboardHeight);
+    this.frets = calculateFrets(this.scaleLength);
 
 
     function calculateStrings(fretboardHeight){
@@ -19,6 +20,21 @@
         strings[i] = { yPosition: (distanceToBorder + i * distanceBetweenStrings) } ;
       }
       return strings;
+    }
+
+    function calculateFrets(scaleLength){
+      var frets = [];
+      var prevDistance = 0;
+
+
+      for(var i = 0; i < 23; i+=1) {
+        var currentDistance = (i == 0 ? 0 : (scaleLength - prevDistance) / 17.817 ) + prevDistance ; // Ternary operator
+
+        frets[i] = { xPosition: currentDistance } ;
+        prevDistance = currentDistance;
+      }
+
+      return frets;
     }
 
   }
