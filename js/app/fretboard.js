383b0fbec1b8023340946000a565ddf1a81cd01b
jgnatch <nachojgutierrez@gmail.com>
2014-11-20 17:38:44 -0300

    added strings

--- js/app/fretboard.js
+++ js/app/fretboard.js
@@ -5,7 +5,25 @@
     this.scaleLength = 1630;
     this.fretboardHeight = 180;
     this.woodLength = 1200;
+
+    this.strings = calculateStrings(this.fretboardHeight);
+
+
+    function calculateStrings(fretboardHeight){
+      var distanceToBorder = 10;
+      var allStringsHeight = fretboardHeight - (distanceToBorder * 2);
+      var distanceBetweenStrings = allStringsHeight / 5;
+
+      var strings = [];
+      for(var i = 0; i < 6; i+=1) {
+        strings[i] = { yPosition: (distanceToBorder + i * distanceBetweenStrings) } ;
+      }
+      return strings;
+    }
+
   }
+
+
 
   app.controller('FretboardCtrl', [FretboardCtrl] );
 
